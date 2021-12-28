c33c3c8002ef890af36ad10cd2aeda1101be8d39
rickydarmawanwidjanarko <63650667+rickydarmawanwidjanarko@users.noreply.github.com>
2021-12-28 13:54:19 +0700

    Add files via upload

---
+++ carakerja.php
@@ -0,0 +1,17 @@
+<?php include_once('_header.php'); ?>
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1>CARA KERJA</h1>
+        <h2><b>Berikut adalah cara kerja penggunaan apliasi (nama aplikasi)</b><br></h2>
+        <p> 1. blablalba<br>
+            2. blablalba<br>
+            3. blablalba<br>
+        </p>
+
+        <a href="fuzzy.php" class="btn btn-primary btn-lg btn-flat" id="menu-toggle">Mulai Hitung &#8594;</a>
+
+    </div>
+</div>
+
+<?php include_once('_foother.php'); ?>
---
+++ penjelasan.php
@@ -0,0 +1,14 @@
+<?php include_once('_header.php'); ?>
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1>PENJELASAN</h1>
+        <h2><b>Fuzzy Mamdani adalah :</b><br></h2>
+        <p> BLABLABLA............ </p>
+
+        <a href="carakerja.php" class="btn btn-primary btn-lg btn-flat" id="menu-toggle">Berikutnya &#8594;</a>
+
+    </div>
+</div>
+
+<?php include_once('_foother.php'); ?>
